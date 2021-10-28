bd3a13a6f1b4df5c05a37e873440360250a28372
ayamir <lgt986452565@gmail.com>
2021-10-28 19:36:19 +0800

    feat: add iterator.

---
+++ primer/iterator.cpp
@@ -0,0 +1,17 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+
+using std::string;
+
+int main(int argc, char *argv[]) {
+  string str = "some string";
+  if (str.begin() != str.end()) {
+    auto it = str.begin();
+    *it = std::toupper(*it);
+  }
+  for (auto b = str.begin(); b != str.end(); ++b) {
+    std::cout << *b;
+  }
+  return 0;
+}
